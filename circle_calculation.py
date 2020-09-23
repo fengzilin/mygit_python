93185b689e13f0cc03afeedafbcb4d791846e564
fengzilin <szkoko_km@163.com>
2020-09-23 22:05:03 +0800

    practies

---
+++ circle_calculation.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+'''
+    圆的周长和面积
+'''
+
+import math
+
+radius = 23
+perimeter = math.pi * radius * 2
+area = math.pi * radius * radius
+print(round(perimeter, 2))
+print(round(area, 2))
